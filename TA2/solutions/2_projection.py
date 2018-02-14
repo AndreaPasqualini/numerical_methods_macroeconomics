e08d1af8e6d0c72d1dcc074d5626e20d82ae4596
Andrea Pasqualini <apsql@users.noreply.github.com>
2018-02-14 14:52:51 +0100

    Started code for solving the exercises

---
+++ TA2/solutions/2_projection.py
@@ -0,0 +1,5 @@
+import numpy as np
+from scipy.optimize import fsolve
+from matplotlib import rc
+from matplotlib import pyplot as plt
+rc('text', usetex=True)  # comment out if LaTeX not on your system
---
+++ TA2/solutions/3_rbc.py
@@ -0,0 +1,4 @@
+import numpy as np
+from matplotlib import rc
+from matplotlib import pyplot as plt
+rc('text', usetex=True)  # comment out if LaTeX not on your system
