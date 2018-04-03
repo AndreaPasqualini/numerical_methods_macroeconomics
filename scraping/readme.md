d019ec0e67c87f86e0282aad5fc1568cea305c2f
Andrea Pasqualini <apsql@users.noreply.github.com>
2018-04-03 14:51:05 +0200

    New folder with web scraping material

---
+++ scraping/readme.md
@@ -0,0 +1,24 @@
+# Web Scraping using BeautifulSoup
+
+The contents in this folder show examples of how to use [BeautifulSoup](https://www.crummy.com/software/BeautifulSoup/) (BS) to scrape web content.
+Note that BeautifulSoup works best with websites that only use HTML code, while websites running Javascript, PHP or similar code might not play nicely with BS, and you might be unable to scrape the information you are aiming for.
+
+
+## XKCD
+The file [xkcd.py](./xkcd.py) shows a simple application of how to use BeautifulSoup to scrape information from the [XKCD website](https://xkcd.com/).
+
+The class `XkcdComic` provides a Python interface to comic-related information: the object is instantiated with the ID number of the comic (found in the URL) and saves the title, the caption and the link to the image file.
+The class has a method that offers to download the image file to local disk.
+
+The code also shows an example of how to use the `XkcdComic` class to download comics and create a simple textual log of the files the user downloads.
+There is also an example of how to be gentle on web servers, waiting some time between requests to prevent excessive server load (and its consequences, e.g., IP address ban).
+
+You can freely use the [xkcd.py](./xkcd.py) file as long as you note [Randall Munroe's License notice](https://xkcd.com/license.html), as he is the creator and the owner of the comics.
+
+
+# Disclaimer
+I want the reader to be aware of the following: web scraping might put you into trouble.
+It might be illegal activity in your Country, violating a website Terms and Conditions or it might simply be considered unethical.
+Whatever you do with the material you find here, make sure you understand the risks and the responsibility that you are taking on.
+I suggest you read [this zine](https://www.crummy.com/software/BeautifulSoup/zine/), written by the developer of BeautifulSoup.
+Let me also remind you that the code you find here is covered by the [MIT license](../LICENSE), meaning in particular that I do not share the responsibility of what _you_ do with my code.
---
+++ scraping/xkcd.py
@@ -0,0 +1,64 @@
+"""
+This code showcases the use of BeautifulSoup for pedagogical purposes. It
+shows the way HTML code is parsed. It gives an idea about the way
+
+"""
+
+from requests import get
+from urllib.request import urlretrieve
+from bs4 import BeautifulSoup
+from time import sleep
+
+
+class XkcdComic:
+    """
+    This class connects to https://xkcd.com/ and fetches data related to
+    comics on that website. The HTML of the page is obtained using
+    requests.get(). Parsing of the HTML code is done using BeautifulSoup.
+    Retrieval of images is done using urllib.request.urlretrieve().
+    The class offers a method to save images on disk. Please note that terms
+    at https://xkcd.com/license.html apply at all times. The images are not
+    my work. Only this code is mine.
+    """
+
+    def __init__(self, comic_number):
+        self.number = str(comic_number)
+        self.url = 'https://xkcd.com/' + self.number + '/'
+        self._webpage = get(self.url)
+        self._soup = BeautifulSoup(self._webpage.text, 'html.parser')
+        self._container = self._soup.find('div', {'id': 'comic'})
+        self._img_url = 'https:' + self._container.img['src']
+        self._png_name = self._img_url.split('/')[-1]
+        self.caption = self._container.img['title']
+        self.title = self._soup.find('div', {'id': 'ctitle'}).text
+
+    def save_img_to_disk(self, directory='./', filename=None):
+        if filename is None:
+            filename = self._img_url.split('/')[-1]
+        if directory[-1] is not '/':
+            directory += '/'
+        urlretrieve(self._img_url, directory + filename)
+
+
+if __name__ == '__main__':
+
+    dir = 'C:/Users/Andrea/Pictures/xkcd/'
+    numbers = [i for i in range(1900, 1968)]
+
+    with open((dir + 'index.txt'), mode='w', encoding='utf-8') as index:
+        index.write('Index of comics (w/captions) \n\n\n')
+
+    for no in numbers:
+
+        comic = XkcdComic(no)
+
+        print('Saving to disk comic no. {}: {}'.format(no, comic.title))
+        fname = '{}-{}'.format(no, comic._png_name)
+        comic.save_img_to_disk(directory=dir,
+                               filename=fname)
+        sleep(1)  # pause execution for 1 second
+
+        with open((dir + 'index.txt'), mode='a', encoding='utf-8') as index:
+            index.write('#{}, {}: {}\n\n'.format(comic.number,
+                                                 comic.title,
+                                                 comic.caption))
