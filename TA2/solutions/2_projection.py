2ac536335afa178b82f327ed3c69fcea4fc5b127
apsql <andpsq@gmail.com>
2018-10-13 00:10:04 +0200

    Reorganized repo

--- TA2/solutions/2_projection.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from scipy.optimize import fsolve
-from matplotlib import rc
-from matplotlib import pyplot as plt
-rc('text', usetex=True)  # comment out if LaTeX not on your system
